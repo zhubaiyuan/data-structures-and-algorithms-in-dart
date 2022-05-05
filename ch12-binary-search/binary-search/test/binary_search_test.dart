50611c53a389e5cae13fb8342d58240426f097cd
Maximalfocus <21332201+zhubaiyuan@users.noreply.github.com>
2022-05-05 23:18:48 +0800

    implementing binary search

---
+++ ch12-binary-search/binary-search/test/binary_search_test.dart
@@ -0,0 +1,27 @@
+import 'package:binary_search/binary_search.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('binarySearch:', () {
+    test('empty list', () {
+      final list = <int>[];
+      expect(list.binarySearch(1), isNull);
+    });
+
+    test('normal list', () {
+      final list = [1, 5, 15, 17, 19, 22, 24, 31, 105, 150];
+      expect(list.binarySearch(1), 0);
+      expect(list.binarySearch(5), 1);
+      expect(list.binarySearch(15), 2);
+      expect(list.binarySearch(17), 3);
+      expect(list.binarySearch(19), 4);
+      expect(list.binarySearch(22), 5);
+      expect(list.binarySearch(24), 6);
+      expect(list.binarySearch(31), 7);
+      expect(list.binarySearch(105), 8);
+      expect(list.binarySearch(150), 9);
+      expect(list.binarySearch(-1), isNull);
+      expect(list.binarySearch(100), isNull);
+    });
+  });
+}
