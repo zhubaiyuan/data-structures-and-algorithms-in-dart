2f19cf21006d0797922434ed6f71eb5b43993c84
Maximalfocus <21332201+zhubaiyuan@users.noreply.github.com>
2022-05-05 15:13:54 +0800

    implement stacks

---
+++ ch04-stacks/stack/bin/stack.dart
@@ -0,0 +1,21 @@
+import 'package:stack/stack.dart';
+
+void main(List<String> arguments) {
+  final stack = Stack<int>();
+  stack.push(1);
+  stack.push(2);
+  stack.push(3);
+  stack.push(4);
+  print(stack);
+
+  final element = stack.pop();
+  print('Popped: $element');
+  print(stack.peek);
+  print(stack.isEmpty);
+  print(stack.isNotEmpty);
+
+  const list = ['S', 'M', 'O', 'K', 'E'];
+  final smokeStack = Stack.of(list);
+  print(smokeStack);
+  print('Popped: ${smokeStack.pop()}');
+}
