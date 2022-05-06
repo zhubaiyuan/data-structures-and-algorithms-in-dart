89abdebc3dd698a75dbffc029e0a149eebe2fb10
Maximalfocus <21332201+zhubaiyuan@users.noreply.github.com>
2022-05-06 18:41:22 +0800

    implementing quick sort

--- ch18-heap-sort/heap-sort/test/heap_sort_test.dart
+++ ch18-heap-sort/heap-sort/test/heap_sort_test.dart
@@ -1,5 +1,6 @@
 import 'package:heap_sort/heap.dart';
 import 'package:heap_sort/heap_sort.dart';
+import 'package:heap_sort/heap_sort_descending.dart';
 import 'package:test/test.dart';
 
 void main() {
@@ -66,6 +67,30 @@ void main() {
       final list = <int>[];
       list.heapsortInPlace();
       expect(list, <int>[]);
+    });
+  });
+
+  group('Challenge 2:', () {
+    test('using reversed', () {
+      final list = [6, 12, 2, 26, 8, 18, 21, 9, 5];
+      final ascending = heapsort(list);
+      expect(list, [6, 12, 2, 26, 8, 18, 21, 9, 5]);
+      expect(ascending, [2, 5, 6, 8, 9, 12, 18, 21, 26]);
+      final descending = ascending.reversed;
+      expect(descending, [26, 21, 18, 12, 9, 8, 6, 5, 2]);
+    });
+
+    test('reimplementing heapsort', () {
+      final list = [6, 12, 2, 26, 8, 18, 21, 9, 5];
+      final sorted = descendingHeapsort(list);
+      expect(list, [6, 12, 2, 26, 8, 18, 21, 9, 5]);
+      expect(sorted, [26, 21, 18, 12, 9, 8, 6, 5, 2]);
+    });
+
+    test('reimplementing heapsortInPlace', () {
+      final list = [6, 12, 2, 26, 8, 18, 21, 9, 5];
+      list.descendingHeapsortInPlace();
+      expect(list, [26, 21, 18, 12, 9, 8, 6, 5, 2]);
     });
   });
 }
