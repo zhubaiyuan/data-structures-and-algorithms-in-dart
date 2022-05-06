39e90b3ee2f5b4b31548e93ee18c501f15a4dea9
Maximalfocus <21332201+zhubaiyuan@users.noreply.github.com>
2022-05-06 21:36:48 +0800

    implementing dijkstras

---
+++ ch23-dijkstras-algorithm/dijkstras/lib/priority_queue.dart
@@ -0,0 +1,27 @@
+import 'heap.dart';
+import 'queue.dart';
+
+export 'heap.dart' show Priority;
+
+class PriorityQueue<E extends Comparable<dynamic>> implements Queue<E> {
+  PriorityQueue({List<E>? elements, Priority priority = Priority.max}) {
+    _heap = Heap<E>(elements: elements, priority: priority);
+  }
+
+  late Heap<E> _heap;
+
+  @override
+  bool get isEmpty => _heap.isEmpty;
+
+  @override
+  E? get peek => _heap.peek;
+
+  @override
+  bool enqueue(E element) {
+    _heap.insert(element);
+    return true;
+  }
+
+  @override
+  E? dequeue() => _heap.remove();
+}
